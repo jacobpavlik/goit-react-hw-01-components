fa84dbcb3643aa8f1b3082c270b1242173b45e88
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-06-28 16:42:56 +0200

    Added profile folder, Profiles.jsx and user.json into it

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import user from './user.json';
+
+export const Profile = props => {
+  const { username, tag, location, avatar, stats } = props;
+  const followers = user.stats.followers;
+  const views = user.stats.views;
+  const likes = user.stats.likes;
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={user.avatar} alt="User avatar" className="avatar" />
+        <p className="name">{user.username}</p>
+        <p className="tag">@{user.tag}</p>
+        <p className="location">{user.location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};

ccf900dffcd7279fae9bad80c58b77fb7b39e5a7
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-06-29 23:14:14 +0200

    Added Statistics.jsx component

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -3,29 +3,22 @@ import PropTypes from 'prop-types';
 export const Statistics = props => {
   const { title, stats } = props;
   return (
-    <div>
-      <section class="statistics">
-        {title && <h2 class="title">{title}</h2>}
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
 
-        <ul class="stat-list">
-          <li class="item">
-            <span class="label">.docx</span>
-            <span class="percentage">4%</span>
+      <ul className="stat-list">
+        {stats.map(stat => (
+          <li key={stat.id} className="item">
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}%</span>
           </li>
-          <li class="item">
-            <span class="label">.mp3</span>
-            <span class="percentage">14%</span>
-          </li>
-          <li class="item">
-            <span class="label">.pdf</span>
-            <span class="percentage">41%</span>
-          </li>
-          <li class="item">
-            <span class="label">.mp4</span>
-            <span class="percentage">12%</span>
-          </li>
-        </ul>
-      </section>
-    </div>
+        ))}
+      </ul>
+    </section>
   );
+};
+Statistics.propTypes = {
+  title: PropTypes.string,
+  label: PropTypes.number,
+  percentage: PropTypes.number,
 };
